81f73dd5e02af1e4c5c16f0a73a37e8d52149f36
Dharma Gupta <dharma.anita@gmail.com>
2026-01-31 21:17:09 -0500

    changed ip address for visualize

--- scripts/visuzalize.py
+++ scripts/visuzalize.py
@@ -4,8 +4,8 @@ import cv2
 import numpy as np
 from logger import CSVLogger
 
-# MQTT_BROKER = "fe80::80ee:98fe:7fcb:95c3%16"
-MQTT_BROKER = "127.0.0.1"
+MQTT_BROKER = "fe80::80ee:98fe:7fcb:95c3%16"
+# MQTT_BROKER = "127.0.0.1"
 CAMERA_TOPIC = "camera/detections"
 PENCIL_TOPIC = "pencil/reading"
 WINDOW_WIDTH = 640
